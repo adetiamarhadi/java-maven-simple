3ed7e3e856fa045e379b375d876e0335d7c77198
Adet <adet@Adetias-MacBook-Pro.local>
2023-09-11 16:05:02 +0700

    complete the project

---
+++ src/test/java/com/github/adetiamarhadi/AppTest.java
@@ -0,0 +1,17 @@
+package com.github.adetiamarhadi;
+
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest {
+
+    @Test
+    public void test() {
+        Assert.assertTrue(true);
+    }
+}
